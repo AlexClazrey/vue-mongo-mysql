65ebf5d2a3e408654c456377a8e321718604c3a9
Alex Claz <vvssuccess@gmail.com>
2018-12-12 14:14:28 +0800

    script modify

--- npmRun.sh
+++ npmRun.sh
@@ -2,13 +2,14 @@
 POS="$(cd "$(dirname "$0")"; pwd -P;)"
 echo "--- Clearing old log file ---"
 cd $POS
-cp /dev/null run.log
+cp /dev/null express.log
+cp /dev/null vue.log
 echo "--- Starting express-server and vue-server with output to run.log ---"
 cd $POS/express-server
-npm start >> ../run.log &
+npm start >> ../express.log &
 cd $POS/vue-server
-npm run serve >> ../run.log &
+npm run serve >> ../vue.log &
 cd $POS
 echo "--- Start watching run.log ---"
-tail -f run.log
+tail -f express.log vue.log
 
