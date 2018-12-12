8a753d7e480643b9cb69a39052637ff73450375a
Alex Claz <vvssuccess@gmail.com>
2018-12-12 13:46:08 +0800

    script modify

--- npmRun.sh
+++ npmRun.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-POS=pwd
+POS="$(cd "$(dirname "$0")"; pwd -P;)"
+echo "--- Clearing old log file ---"
+cd $POS
+cp /dev/null run.log
+echo "--- Starting express-server and vue-server with output to run.log ---"
 cd $POS/express-server
 npm start >> ../run.log &
 cd $POS/vue-server
 npm run serve >> ../run.log &
 cd $POS
+echo "--- Start watching run.log ---"
 tail -f run.log
 
--- vue-server/config/config.js
+++ vue-server/config/config.js
@@ -1,3 +1,5 @@
+// This file will be delivered to user browser
+// so no confidential infomartions in it.
 export default {
 	webApi: {
 		address: "http://localhost:8081/"
