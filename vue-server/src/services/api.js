db108f2cfd3e6ef56dd83219b9e349121bca5822
alexclaz <vvssuccess@live.com>
2018-12-14 13:47:53 +0800

    config change

--- vue-server/src/services/api.js
+++ vue-server/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import conf from '@/../config.js';
+var conf = require('@/../config.js');
+
 export default() => {
 	return axios.create({
 		baseURL: conf.apiServer.address
