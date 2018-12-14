045cf3b4504a19a93ee5c57ae7f1ca9c6fde76db
RickyXu <raxrh1997@163.com>
2018-12-14 20:43:37 +0800

    shell debug

--- npmInstall.sh
+++ npmInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 POS="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $POS/express-server
+cd "$POS"/express-server
 npm install
-cd $POS/vue-server
+cd "$POS"/vue-server
 npm install
-cd $POS
+cd "$POS"
 echo "[INFO] MongoDB and MySQL connection config is in express-server/config.json."
 echo "[INFO] If you want to accept public traffic, you need change public ip address in vue-server/config.js.";
 echo "[INFO] Use 'npm start' in express-server and 'npm run serve' in vue-server to start."
--- npmRun.sh
+++ npmRun.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 POS="$(cd "$(dirname "$0")"; pwd -P;)"
 echo "--- Clearing old log file ---"
-cd $POS
+cd "$POS"
 cp /dev/null express.log
 cp /dev/null vue.log
 echo "--- Starting express-server and vue-server with output to run.log ---"
-cd $POS/express-server
+cd "$POS"/express-server
 npm start >> ../express.log &
-cd $POS/vue-server
+cd "$POS"/vue-server
 npm run serve >> ../vue.log &
-cd $POS
+cd "$POS"
 echo "--- Start watching run.log ---"
 tail -f express.log vue.log
 
