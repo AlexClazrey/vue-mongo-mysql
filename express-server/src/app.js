94dc1218596d739f34fc08e3ce52111c3ad10a7e
Alex Claz <vvssuccess@gmail.com>
2018-12-14 18:52:44 +0800

    mongo auth debug

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -9,8 +9,12 @@ const PostRouter = require('./router/post-router-mysql');
 
 // read config and set up mongo connection
 const mongoAddr = conf.mongodb.host + ':' + conf.mongodb.port + '/' + conf.mongodb.db;
-const mongoUser = conf.mongodb.auth ? (conf.mongodb.username + ':' + conf.mongodb.password + '@') : '';
-mongoose.connect('mongodb://' + mongoUser + mongoAddr, { useNewUrlParser: true });
+if(conf.mongodb.auth) {
+    const mongoUser = conf.mongodb.auth ? (conf.mongodb.username + ':' + conf.mongodb.password + '@') : '';
+    mongoose.connect('mongodb://' + mongoUser + mongoAddr, { useNewUrlParser: true, auth: {authdb: 'admin'}});
+} else {
+    mongoose.connect('mongodb://' + mongoAddr, { useNewUrlParser: true });
+}
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "[Error][MongoDB] Connection Error."));
 db.once("open", console.log.bind(console, "[OK][MongoDB] Connection succeeded."));
