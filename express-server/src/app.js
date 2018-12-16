c8dd771c27da31670f5ef26ff82b3975aa45cb26
Alex Claz <vvssuccess@gmail.com>
2018-12-16 21:27:56 +0800

    remove mongodb and disable test

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -5,19 +5,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const conf = require('./readConfig');
-const PostRouter = require('./router/post-router-mysql');
-
-// read config and set up mongo connection
-const mongoAddr = conf.mongodb.host + ':' + conf.mongodb.port + '/' + conf.mongodb.db;
-if(conf.mongodb.auth) {
-	console.log(conf);
-    mongoose.connect('mongodb://' + mongoAddr, { user: conf.mongodb.username, pass: conf.mongodb.password, auth: {authdb: 'admin'}});
-} else {
-    mongoose.connect('mongodb://' + mongoAddr, { useNewUrlParser: true });
-}
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "[Error][MongoDB] Connection Error."));
-db.once("open", console.log.bind(console, "[OK][MongoDB] Connection succeeded."));
+// const PostRouter = require('./router/post-router-mysql');
 
 // set up express server
 const app = express();
@@ -26,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // add routers here
-app.use('/', PostRouter);
+// app.use('/', PostRouter);
 
 // listen
 app.listen(process.env.PORT || 8081);
