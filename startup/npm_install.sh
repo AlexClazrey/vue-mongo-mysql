acc18cfa604c9caae24e81924dd477a6ccf9c8f1
Alex Claz <vvssuccess@gmail.com>
2019-05-19 22:26:23 +0800

    echo message fix

--- startup/npm_install.sh
+++ startup/npm_install.sh
@@ -8,4 +8,4 @@ cd "$POS"
 echo "[INFO] MongoDB and MySQL connection config is in express-server/config.json ."
 echo "[INFO] If you want to accept public traffic, you need change public ip address in vue-server/config.json .";
 echo "[INFO] Use 'npm start' in express-server and 'npm run serve' in vue-server to start."
-echo "[INFO] npmRun.sh packed two commands above."
+echo "[INFO] run_linux.sh packed two commands above."
--- startup/yarn_install.sh
+++ startup/yarn_install.sh
@@ -10,6 +10,6 @@ echo
 echo "[INFO] MongoDB and MySQL connection config is in /express-server/config.json ."
 echo "[INFO] If you want to accept public traffic, you need change public ip address in /vue-server/config.json .";
 echo "[INFO] Use 'npm start' in express-server and 'npm run serve' in vue-server to start."
-echo "[INFO] npmRun*.sh packed two commands above."
-echo "[INFO] choose npmRun_cygwin.sh on Windows, or npmRun_linux.sh on Linux."
+echo "[INFO] run*.sh packed two commands above."
+echo "[INFO] choose run_cygwin.sh on Windows, or run_linux.sh on Linux."
 
