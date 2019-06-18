117bf587cb7fd29b95eac0593f9d9cc026b62904
Alex Claz <vvssuccess@gmail.com>
2019-06-18 17:59:09 +0800

    protocol from browser url

--- vue-server/src/services/api.js
+++ vue-server/src/services/api.js
@@ -3,9 +3,10 @@ import conf from '@/../config.json';
 
 export default(prefix) => {
 	var host = document.baseURI.substr(document.baseURI.indexOf('://') + 3);
-	host = host.substring(0, host.indexOf('/'));
+	host = host.substring(0, host.indexOf('/') - 1);
+	var protocol = document.baseURI.substr(0, document.baseURI.indexOf('://')) + '://';
 	var apiAddress = 
-		conf.apiServer.protocol
+		protocol
 		+ host + '/'
 		+ conf.apiServer.prefix;
 	if(prefix) {
